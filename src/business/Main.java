da8c6d6806ae35391abe5230e643d5fd424c50fb
Gabriel Moya <itsgamc@gmail.com>
2026-01-23 09:18:16 -0500

    Ejemplo 0

---
+++ src/business/Main.java
@@ -0,0 +1,13 @@
+package business;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int result = Test.multiplicacion(5, 10);
+		System.out.println("El resultado de la multiplicacion es: " + result);
+				
+	}
+	
+}
+
---
+++ src/business/Test.java
@@ -0,0 +1,9 @@
+package business;
+
+public class Test {
+
+	
+	public static int multiplicacion(int a, int b) {
+		return a*b;
+	}
+}
